409cd09ee9d5540fb89b5f8d8ab64ee1e91e751c
Saku703 <saku.ikonen1@gmail.com>
2021-04-14 15:02:52 +0300

    Added Hello world

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
+        System.out.println("Hello world!");
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
